800e650a335133ead3afe8b4eb8a20a2b3dfbe04
Erika Johansson <erika.johansson@etteplan.com>
2020-12-04 16:02:25 +0100

    Day4 Refactor AoC 2020

--- 2020_AdventOfCode/day4.py
+++ 2020_AdventOfCode/day4.py
@@ -1,5 +1,4 @@
 import sys
-import time
 import re
 from utils import readFile
 
@@ -7,7 +6,6 @@ def organizeItem(item):
     entry = item.split(" ")
     collection = {tuple(row.split(":")) for row in entry}
     return dict(collection)
-    
 
 def extractPassport(content):
     passportInfoCollection = []
@@ -21,12 +19,15 @@ def extractPassport(content):
 
     return passportInfoCollection
 
-def isDigits(s):
-    pattern = "\d+"
-    expression = re.compile(pattern)
-    if expression.fullmatch(s):
+
+def checkPattern(s, regex):
+    if regex.fullmatch(s):
         return True
     return False
+    
+
+def isDigits(s):
+    return checkPattern(s, re.compile("\d+"))
 
 def isBetween(x, lowLim, highLim):
     return x >= lowLim and x <= highLim
@@ -45,56 +46,38 @@ def isValidHeight(height):
     
     return False
 
-def isValidColor(color):
-    pattern = "#[0-9a-f]{6}"
-    expression = re.compile(pattern)
-    if expression.fullmatch(color):
-        return True
-    return False
+def isValidHair(color):
+    return checkPattern(color, re.compile("#[0-9a-f]{6}"))
 
 def isValidEye(eye):
-    pattern = "(amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth)"
-    expression = re.compile(pattern)
-    if expression.fullmatch(eye):
-        return True
-    return False
+    return eye in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
 
 def isValidPID(pid):
-    pattern = "\d{9}"
-    expression = re.compile(pattern)
-    if expression.fullmatch(pid):
-        return True
-    return False
+    return checkPattern(pid,re.compile("\d{9}"))
+
+def isValidBirth(birth):
+    return isDigits(birth) and isBetween(int(birth), 1920, 2003)
+        
+def isValidIssue(issue):
+    return isDigits(issue) and isBetween(int(issue), 2010, 2020)
+    
+def isValidExpiration(expiration):
+    return isDigits(expiration) and isBetween(int(expiration), 2020, 2030)
 
 
+def validateFields(item):
+    return all([
+        isValidBirth(item["byr"]),
+        isValidExpiration(item["eyr"]),
+        isValidEye(item["ecl"]),
+        isValidHair(item["hcl"]),
+        isValidHeight(item["hgt"]),
+        isValidIssue(item["iyr"]),
+        isValidPID(item["pid"])
+    ])
 
-def checkValidationFields(item):
-    if not isDigits(item["byr"]) or not isBetween(int(item["byr"]), 1920, 2003):
-        return False
-    if not isDigits(item["iyr"]) or not isBetween(int(item["iyr"]), 2010, 2020):
-        return False
-    if not isDigits(item["eyr"]) or not isBetween(int(item["eyr"]), 2020, 2030):
-        return False
-    if not isValidHeight(item["hgt"]):
-        return False
-    if not isValidColor(item["hcl"]):
-        return False
-    if not isValidEye(item["ecl"]):
-        return False
-    if not isValidPID(item["pid"]):
-        return False
-    return True
-
-def containsFields(item, fields, validation=False):
-    result = True
-    for field in fields:
-        if field not in item:
-            result = False
-    if validation:
-        result = checkValidationFields(item)
-        print(result)
-    return result
-
+def containsFields(item, fields):
+    return all(key in item for key in fields)
 
 def main():
     fileName = sys.argv[1]
@@ -107,14 +90,13 @@ def main():
     validField = 0
     for passportInfo in passportInfoCollection:
         if containsFields(passportInfo, FIELDS):
-            print(passportInfo)
             count = count + 1
-            if containsFields(passportInfo, FIELDS, validation=True):
+            if validateFields(passportInfo):
                 validField = validField + 1
 
     print("Total number of passports: {0}".format(len(passportInfoCollection)))
-    print("Number passports with all field: {0}".format(count))
-    print("Number passports with all valid:  {0}".format(validField))
+    print("Number passports with all fields present: {0}".format(count))
+    print("Number passports with all fields valid:  {0}".format(validField))
 
 if __name__ == '__main__':
     main()
